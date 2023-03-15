cd7304ac052804aded3b98d4a96fc3b2f18dcb2a
kshitij12345 <kshitijkalambarkar@gmail.com>
2023-03-15 08:40:44 +0000

    update tests

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,109 +1,37 @@
 from __future__ import print_function
 
-import argparse
+try:
+    # neptune-client>=1.0.0 package structure
+    from neptune import init_run
+except ImportError:
+    # neptune-client=0.9.0+ package structure
+    from neptune.new import init_run
 
-import neptune.new as neptune
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import (
-    datasets,
-    transforms,
-)
 
 from neptune_pytorch import NeptuneLogger
 
-# from npt_logger import NeptuneLogger
 
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
-
-
-def main():
+def test_e2e(model, dataset):
     # Training settings
-    parser = argparse.ArgumentParser(description="PyTorch MNIST Example")
-    parser.add_argument(
-        "--batch-size", type=int, default=2048, metavar="N", help="input batch size for training (default: 64)"
-    )
-    parser.add_argument(
-        "--test-batch-size", type=int, default=1000, metavar="N", help="input batch size for testing (default: 1000)"
-    )
-    parser.add_argument("--epochs", type=int, default=3, metavar="N", help="number of epochs to train (default: 14)")
-    parser.add_argument("--lr", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)")
-    parser.add_argument("--gamma", type=float, default=0.7, metavar="M", help="Learning rate step gamma (default: 0.7)")
-    parser.add_argument("--no-cuda", action="store_true", default=False, help="disables CUDA training")
-    parser.add_argument("--no-mps", action="store_true", default=False, help="disables macOS GPU training")
-    parser.add_argument("--dry-run", action="store_true", default=False, help="quickly check a single pass")
-    parser.add_argument("--seed", type=int, default=1, metavar="S", help="random seed (default: 1)")
-    parser.add_argument(
-        "--log-interval",
-        type=int,
-        default=10,
-        metavar="N",
-        help="how many batches to wait before logging training status",
-    )
-    parser.add_argument("--save-model", action="store_true", default=False, help="For Saving the current Model")
-    args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-    use_mps = not args.no_mps and torch.backends.mps.is_available()
+    device = torch.device("cpu")
+    train_kwargs = {"batch_size": 8}
 
-    torch.manual_seed(args.seed)
+    dataset = torch.utils.data.TensorDataset(*dataset)
+    train_loader = torch.utils.data.DataLoader(dataset, **train_kwargs)
 
-    if use_cuda:
-        device = torch.device("cuda")
-    elif use_mps:
-        device = torch.device("mps")
-    else:
-        device = torch.device("cpu")
+    model = model.to(device)
+    optimizer = optim.Adadelta(model.parameters(), lr=0.001)
 
-    train_kwargs = {"batch_size": args.batch_size}
-    test_kwargs = {"batch_size": args.test_batch_size}
-    if use_cuda:
-        cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": True}
-        train_kwargs.update(cuda_kwargs)
-        test_kwargs.update(cuda_kwargs)
-
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-
-    data_dir = "../data"
-    dataset1 = datasets.MNIST(data_dir, train=True, download=True, transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
-
-    model = Net().to(device)
-    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-
-    run = neptune.init_run()
+    run = init_run()
 
     npt_logger = NeptuneLogger(
-        run, model=model, log_model_diagram=True, log_gradients=True, log_parameters=True, log_freq=20
+        run, model=model, log_model_diagram=True, log_gradients=True, log_parameters=True, log_freq=3
     )
-    run[npt_logger.base_namespace]["datasets"].track_files(data_dir)
 
-    for epoch in range(1, args.epochs + 1):
+    for epoch in range(1, 4):
         model.train()
         for batch_idx, (data, target) in enumerate(train_loader):
             data, target = data.to(device), target.to(device)
@@ -113,16 +41,6 @@ def main():
 
             loss.backward()
             optimizer.step()
-            if batch_idx % args.log_interval == 0:
-                print(
-                    "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
-                        epoch,
-                        batch_idx * len(data),
-                        len(train_loader.dataset),
-                        100.0 * batch_idx / len(train_loader),
-                        loss.item(),
-                    )
-                )
 
             run[npt_logger.base_namespace]["batch/loss"].append(loss.item())
 
@@ -130,7 +48,3 @@ def main():
 
     # Save final model
     npt_logger.save_model("model")
-
-
-if __name__ == "__main__":
-    main()
