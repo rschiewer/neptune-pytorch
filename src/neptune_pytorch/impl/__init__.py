941b99b1c0a013c7e78b82886a5c76d7ffe21eed
Sabine <sabine.nyholm@neptune.ai>
2023-08-16 10:58:53 +0300

    add docstrings for log_model() and log_checkpoint()

--- src/neptune_pytorch/impl/__init__.py
+++ src/neptune_pytorch/impl/__init__.py
@@ -184,6 +184,19 @@ class NeptuneLogger:
         return self._base_namespace
 
     def log_model(self, model_name: Optional[str] = None):
+        """Uploads the model to Neptune, into a namespace called "model" inside the run.
+
+        The filename is set to `model.pt` by default, but can be customized.
+
+        Args:
+            model_name: Name for the logged model file. The extension `.pt` is added automatically.
+
+        Example:
+            from neptune_pytorch import NeptuneLogger
+            neptune_logger = NeptuneLogger()
+            ...
+            neptune_logger.log_model()
+        """
         if model_name is None:
             # Default model name
             model_name = "model.pt"
@@ -194,6 +207,23 @@ class NeptuneLogger:
         safe_upload_model(self._namespace_handler["model"], model_name, self.model)
 
     def log_checkpoint(self, checkpoint_name: Optional[str] = None):
+        """Uploads a model checkpoint to Neptune, into a namespace called "model/checkpoints" inside the run.
+
+        The filename is set to `checkpoint_<checkpoint number>.pt` by default, but can be customized.
+
+        Args:
+            checkpoint_name: Name for the logged checkpoint file.
+                If left empty, the checkpoint number used for the file name starts from 1 and is incremented
+                automatically on each call. The extension `.pt` is added automatically.
+
+        Example:
+            from neptune_pytorch import NeptuneLogger
+            neptune_logger = NeptuneLogger()
+            ...
+            for epoch in range(parameters["epochs"]):
+                ...
+                neptune_logger.log_checkpoint()
+        """
         if checkpoint_name is None:
             # Default checkpoint name
             checkpoint_name = f"checkpoint_{self.ckpt_number}.pt"
